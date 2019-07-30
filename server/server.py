ed5720411bec9aebded46e8169b15ed08517807e
Emanuel Zgraggen <emanuel.zgraggen@gmail.com>
2019-07-30 17:09:47 -0400

    added simple flask server that allows to send cleaning command over the web

--- server/server.py
+++ server/server.py
@@ -20,9 +20,9 @@ filename = "../dirtydata/property.csv"
 df_orig = pd.read_csv(filename)
 
 
-# example commands to send are like this
-# eval("dcf.create_limiting_factor(df, 'PID', 'digits')")
-# eval("dcf.keep_rows_with_nonenan_within_col(df, 'MOVE_IN_DATE')")
+# example commands array to send in body of post
+# [ "dcf.create_limiting_factor(df, 'PID', 'digits')", "dcf.keep_rows_with_nonenan_within_col(df, 'MOVE_IN_DATE')" ]
+
 @app.route("/cleaned", methods=["POST"])
 def customddataset():
     commands = request.get_json()
@@ -34,6 +34,9 @@ def customddataset():
     for c in commands:
         eval(c)
 
+    # reset column headers to just name
+    df.columns = [c[0] for c in df.columns]
+
     return Response(df.to_json(), mimetype='application/json')
 
 @app.route("/original", methods=["GET"])
@@ -43,5 +46,3 @@ def original():
 
 if __name__ == "__main__":
     app.run("0.0.0.0", 9050, threaded=True)
-
-
